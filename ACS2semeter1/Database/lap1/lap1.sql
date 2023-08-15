683ba1d0e3741d3259f392742d517d55bda2488a
Narutchai <koonfluk2015@gmail.com>
2023-08-15 17:47:25 +0700

    fianl SQL

--- ACS2semeter1/Database/lap1/lap1.sql
+++ ACS2semeter1/Database/lap1/lap1.sql
@@ -27,11 +27,38 @@ foreign key (park_code) references themepark(park_code)
 );
 
 create table attraction (
-attract_no numeric(10) auto_increment primary key,
+attract_no numeric(10) primary key,
 park_code varchar(10) not null,
 attract_name varchar(35) not null,
 attract_age numeric(3) default 0,
 attract_capacity numeric(3) not null
 );
 
+
+create table hour (
+emp_num numeric(4) ,
+attract_no numeric(10) ,
+hour_per_attract numeric(2) not null,
+hour_rate numeric(4,2) not null,
+date_worked date not null,
+foreign key(emp_num) references employee(emp_num),
+foreign key(attract_no) references attraction(attract_no)
+);
+
+create table sales (
+transaction_no numeric(10) primary key,
+park_code varchar(10) not null,
+sale_date date not null,
+foreign key (park_code) references themepark(park_code)
+);
+
+create table salesline (
+transaction_no numeric(10),
+line_no numeric(2) not null,
+ticket_no numeric(10) not null,
+line_qty numeric(4) not null,
+line_price numeric(9,2) not null,
+foreign key (transaction_no) references sales(transaction_no),
+foreign key (ticket_no) references ticket(ticket_no)
+);
 
